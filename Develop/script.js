08117e4e4b44d8411b3281f1a5b0ec9e7ad70014
Doctor Mud <davidmueth@gmail.com>
2021-08-26 22:17:03 -0500

    random password generated

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword(){
-    let lowerOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  // Setting up available characters in arrays
+  let lowerOptions = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     let upperOptions = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    let numOptions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let numOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     let spcOptions = ['!', '@', '#', '$', '%', '&', '*', '?', '+', '-', '^'];
     let pool = [];
-
-    let passLength = prompt("How long would you like your password to be? Enter a number between 8 and 128: ")
+// Prompt password length and validate entry.
+    let passLength = prompt("How long would you like your password to be? Enter a number between 8 and 128:")
+    passLength = Number(passLength);
+    if (passLength === null || passLength === ""){
+      alert("To proceed, you must enter a value between 8 and 128!");
+      return generatePassword();
+    }
+    if (passLength < 8 || passLength > 128){
+      alert("Password must be at least 8 and no more than 128 characters in length.");
+      return generatePassword();
+    }
+    // Prompt user for which sets of characters they would like to include
     let lowerChar = confirm("Would you like your password to include lowercase letters?");
     if (lowerChar) {
       pool = pool.concat(lowerOptions);
@@ -26,17 +37,18 @@ function generatePassword(){
     if (specialChar) {
       pool = pool.concat(spcOptions);
     }
-    console.log(pool);
-    console.log(pool.length);
+
     console.log(Math.floor(Math.random()* pool.length));
-    for (let i=0; i < passLength.length; i++){
-      let tmpPass = " ";
-      tmpPass = tmpPass + Math.floor(Math.random()* pool.length);
-      console.log(tmpPass);
-      return tmpPass;
+    let tmpPass = " ";
+    for (let i=0; i < passLength; i++){
+      let n = Math.floor(Math.random()*pool.length);
+      tmpPass += pool[n];
     }
-    
     console.log("Voila! Here is your new password!");
+    return tmpPass;
+    
+    
+    
   
 };
 
